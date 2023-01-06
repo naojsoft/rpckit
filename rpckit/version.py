da06098d1471a4b3c79a2bf33e512b5fc81f2dd1
Eric Jeschke <eric@naoj.org>
2023-01-05 15:38:09 -1000

    Move to setuptools_scm, pyproject.toml and setup.cfg

--- rpckit/version.py
+++
@@ -1,5 +0,0 @@
-major = 1
-minor = 1
-release = 20220323100000
-
-version = '%d.%d.%d' % (major, minor, release)
--- setup.py
+++ setup.py
@@ -1,35 +1,5 @@
 #! /usr/bin/env python
 #
-try:
-    from setuptools import setup
+from setuptools import setup
 
-except ImportError:
-    from distutils.core import setup
-from rpckit.version import version
-import os
-
-srcdir = os.path.dirname(__file__)
-
-from distutils.command.build_py import build_py
-
-def read(fname):
-    buf = open(os.path.join(srcdir, fname), 'rt').read()
-    return buf
-
-setup(
-    name = "rpckit",
-    version = version,
-    author = "Software Division, Subaru Telescope, National Astronomical Observatory of Japan",
-    author_email = "ocs@naoj.org",
-    description = ("Python package for making SunRPC compatible calls."),
-    long_description = read('README.txt'),
-    license = "GPL",
-    keywords = "NAOJ, subaru, telescope, instrument, data",
-    url = "http://naojsoft.github.com/rpckit",
-    packages = ['rpckit'],
-    package_data = { },
-    scripts = ['scripts/rpcgen.py'],
-    classifiers = [
-    ],
-    cmdclass={'build_py': build_py}
-)
+setup()
